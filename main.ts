353038fc1631cc0776a40d7ba415b4f6a2da5037
jauplant-cmd <jauplant@kippsters.kippma.org>
2026-05-15 09:42:49 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+// the 'while' loop of course
+// repeated code right here
+while (true) {
+	
+}
+// the for loop is here
+// repeated code right here
+for (let index = 0; index <= 4; index++) {
+	
+}
+// this is a 'repeat' loop, really!
+// repeated code right here
+for (let index = 0; index < 4; index++) {
+	
+}
